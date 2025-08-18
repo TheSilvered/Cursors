8bb21ed712d04c274b84c5623b266da2a861ce63
Davide Taffarello <taffarello.da@gmail.com>
2025-08-19 00:53:13 +0200

    Separated cursor data from generator

--- generate.py
+++ generate.py
@@ -77,7 +77,7 @@ Offset  Size  Name       Description
 ------------------------------------
      0     4  cbSizeof   Size of the header (= 36)
      4     4  cFrames    Number of frames in the list
-     8     4  cSteps     Number of frames in the animation (the same as cFrames if not SEQUENCE is not set)
+     8     4  cSteps     Number of frames in the animation (the same as cFrames if SEQUENCE is not set)
     12     4  cx         Not used, = 0
     16     4  cy         Not used, = 0
     20     4  cBitCount  Not used, = 0
@@ -118,10 +118,11 @@ The pixel data can either be in BMP format or in PNG format.
 # TODO: add support for .ani cursors
 
 import asyncio
+from dataclasses import dataclass
 import os
 import os.path
 import shutil
-from typing import Collection
+from typing import Collection, NoReturn
 import xml.etree.ElementTree as xml
 
 # Utility functions
@@ -132,12 +133,205 @@ def u8(x: int) -> bytes: return x.to_bytes(1, "little")
 
 def gray(s: str) -> str: return "\x1b[90m" + s + "\x1b[0m"
 def red(s: str) -> str: return "\x1b[91m" + s + "\x1b[0m"
+def yellow(s: str) -> str: return "\x1b[33m" + s + "\x1b[0m"
 
 try:
     from PIL import Image
 except ImportError as e:
     e.add_note(red("The 'pillow' package must be installed."))
     raise e
+
+
+class CursorError(Exception):
+    """Exception raised when creating a `Cursor` object fails."""
+    pass
+
+
+class Cursor:
+    @dataclass
+    class AniCfg:
+        frame_count: int
+        frame_rate: int
+        frame_list: list[int] | None
+        rate_list: list[int] | None
+
+    @dataclass
+    class Hotspot:
+        x: float
+        y: float
+
+    def __init__(self, path: str):
+        self.path = path
+        if self.path[-4:] != ".svg":
+            self.__error("expected an SVG image")
+        if not os.path.isfile(path):
+            self.__error(f"{path} is not a file")
+        self.name = os.path.basename(path)[:-4]
+        self.hotspot, self.ani_cfg = self.__get_info()
+
+    def is_ani(self):
+        return self.ani_cfg is not None
+
+    def __warning(self, msg: str) -> None:
+        print(yellow(f"{self.path}: {msg}"))
+
+    def __error(self, msg: str) -> NoReturn:
+        print(red(f"{self.path}: {msg}"))
+        raise CursorError(f"{self.path}: {msg}")
+
+    def __get_info(self) -> tuple[Hotspot, AniCfg | None]:
+        svg_tree = xml.parse(self.path).getroot()
+        width_attr = svg_tree.attrib.get("width")
+        height_attr = svg_tree.attrib.get("height")
+
+        x_attr = None
+        y_attr = None
+        ani_cfg_str = None
+
+        for element in svg_tree:
+            if element.get('id') == 'hotspot' and element.tag.endswith('rect'):
+                x_attr = element.get('x')
+                y_attr = element.get('y')
+            elif element.get('id') == 'ani_config' and element.tag.endswith('text'):
+                ani_cfg_str = element.text
+
+        hotspot = self.__parse_hotspot(width_attr, height_attr, x_attr, y_attr)
+        ani_cfg = self.__parse_ani_cfg(ani_cfg_str) if ani_cfg_str is not None else None
+
+        return hotspot, ani_cfg
+
+    def __parse_hotspot(
+            self,
+            width_attr: str | None,
+            height_attr: str | None,
+            x_attr: str | None,
+            y_attr: str | None
+    ) -> Hotspot:
+        if width_attr is None or height_attr is None:
+            self.__error("failed to query SVG size")
+        try:
+            width = int(width_attr)
+            height = int(height_attr)
+        except ValueError:
+            self.__error("failed to query SVG size")
+
+        if x_attr is None or y_attr is None:
+            self.__warning("missing hotspot")
+
+        if x_attr is None:
+            x = 0
+        else:
+            try:
+                x = int(x_attr)
+            except ValueError:
+                self.__warning("invalid hotspot X position")
+                x = 0
+
+        if y_attr is None:
+            y = 0
+        else:
+            try:
+                y = int(y_attr)
+            except ValueError:
+                self.__warning("invalid hotspot Y position")
+                y = 0
+
+        if x < 0 or y < 0 or x > width or y > height:
+            self.__warning("the hotspot is outside of the drawing")
+
+        x = min(max(x / width, 0), 1)
+        y = min(max(y / height, 0), 1)
+
+        return self.Hotspot(x, y)
+
+    def __parse_ani_cfg(self, cfg_str: str) -> AniCfg:
+        items = cfg_str.strip().removesuffix(";").split(";")
+        str_cfg = {}
+
+        opt_name: str
+        str_value: str
+
+        # Parse the key-value pairs
+
+        for item in items:
+            if "=" not in item:
+                self.__warning(f"option is missing value '{item}', format: optionName=value")
+                continue
+            opt_name, str_value = item.split("=")
+            str_cfg[opt_name.strip()] = str_value.strip()
+
+        final_cfg = {}
+
+        # Parse the values themselves
+
+        for opt_name, str_value in str_cfg.items():
+            if opt_name in ("frameCount", "frameRate"):
+                try:
+                    value = map(self.__parse_int, str_value.removesuffix(",").split(","))
+                except ValueError:
+                    self.__warning(f"invalid value '{str_value}' for option '{opt_name}'")
+                    continue
+            elif opt_name in ("frameList", "rateList"):
+                try:
+                    value = self.__parse_int(str_value)
+                except ValueError:
+                    self.__warning(f"invalid value '{str_value}' for option '{opt_name}'")
+                    continue
+            else:
+                self.__warning(f"unknown option '{opt_name}'")
+                continue
+
+            final_cfg[opt_name] = value
+
+        # Check for correctness in the values themselves
+
+        if "frameCount" not in final_cfg:
+            self.__error(f"missing required option 'frameCount'")
+
+        if "frameList" in final_cfg:
+            frame_list = final_cfg["frameList"]
+            for frame in frame_list:
+                if frame >= final_cfg["frameCount"]:
+                    self.__error(f"frame index {frame} is too big")
+
+        if final_cfg.get("frameRate", 1) == 0:
+            self.__warning("'frameRate' cannot be zero")
+            final_cfg["frameRate"] = 1
+
+        expected_rate_len = len(final_cfg["frameList"]) if "frameList" in final_cfg else final_cfg["frameCount"]
+        if "rateList" in final_cfg:
+            rate_list: list[int] = final_cfg["rateList"]
+            zero_rate = False
+            for i, rate in enumerate(rate_list):
+                if rate == 0:
+                    zero_rate = True
+                    rate_list[i] = 1
+
+            if zero_rate:
+                self.__warning("no rate in 'rateList' can be zero")
+
+            if len(rate_list) != expected_rate_len:
+                self.__warning(f"'rateList' was expected to have {expected_rate_len} elements but had {len(rate_list)}")
+
+            if len(rate_list) < expected_rate_len:
+                rate_list.extend([final_cfg.get("frameRate", 1)] * (expected_rate_len - len(rate_list)))
+            elif len(rate_list) > expected_rate_len:
+                final_cfg["rateList"] = rate_list[:expected_rate_len]
+
+        return self.AniCfg(
+            frame_count=final_cfg["frameCount"],
+            frame_rate=final_cfg.get("frameRate", 1),
+            frame_list=final_cfg.get("frameList"),
+            rate_list=final_cfg.get("rateList")
+        )
+
+    @staticmethod
+    def __parse_int(s: str) -> int:
+        value = int(s)
+        if value < 0 or value >= 2**32:
+            raise ValueError
+        return value
+
 
 class CursorGenerator:
     """
@@ -169,28 +363,21 @@ class CursorGenerator:
     Needs Inkscape. The name of the cursor is taken from the source file.
     The intermediate PNGs are store in 'png_out_dir'.
     """
-    def __init__(self, src_svg: str, png_out_dir: str, cur_out_dir: str, res: Collection[int] = (32, 48, 64)):
-        if src_svg[-4:] != ".svg":
-            raise TypeError("expected an SVG image")
-        if not os.path.exists(src_svg):
-            raise FileNotFoundError(f"{src_svg} not found")
-
-        self.src_svg = src_svg
-        self.name = os.path.basename(self.src_svg)[:-4]
-        self.png_out_dir = os.path.join(png_out_dir, self.name)
+    def __init__(self, src: Cursor, png_out_dir: str, cur_out_dir: str, resolutions: Collection[int] = (32, 48, 64)):
+        self.src = src
+        self.png_out_dir = os.path.join(png_out_dir, self.src.name)
         self.cur_out_dir = cur_out_dir
-        self.res = res
-        self.is_animated = src_svg[-8:] == ".ani.svg"
+        self.resolutions = resolutions
 
-    async def __gen_pngs(self):
-        src_file_mtime = os.path.getmtime(self.src_svg)
+    async def __gen_pngs(self) -> bool:
+        src_file_mtime = os.path.getmtime(self.src.path)
 
         os.makedirs(self.png_out_dir, exist_ok=True)
 
         actions = []
         out_files = []
 
-        for res in self.res:
+        for res in self.resolutions:
             out_file = os.path.join(self.png_out_dir, f"{res}.png")
             # Only generate the file if the SVG is newer than the PNG
             if os.path.exists(out_file) and os.path.getmtime(out_file) > src_file_mtime:
@@ -206,13 +393,13 @@ class CursorGenerator:
             out_files.append(out_file)
 
         if len(out_files) == 0:
-            return
+            return True
 
         print(f"Generating {', '.join(out_files)}...")
 
         process = await asyncio.create_subprocess_exec(
             "inkscape",
-            self.src_svg,
+            self.src.path,
             "--actions=" + ";".join(actions),
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE
@@ -220,14 +407,16 @@ class CursorGenerator:
         stdout, stderr = await process.communicate()
 
         if process.returncode != 0 or not all(map(os.path.exists, out_files)):
-            exc = RuntimeError(f"Generation of {', '.join(out_files)} failed")
-            exc.add_note("Stderr: " + stderr.decode().strip())
-            exc.add_note("Stdout: " + stdout.decode().strip())
-            raise exc
+            print(red(f"Generation of {', '.join(out_files)} failed"))
+            print(gray("stderr: " + stderr.decode().strip()))
+            print(gray("stdout: " + stdout.decode().strip()))
+            return False
+
+        return True
 
     def __gen_cur(self):
-        src_file_mtime = os.path.getmtime(self.src_svg)
-        out_file = os.path.join(self.cur_out_dir, f"{self.name}.cur")
+        src_file_mtime = os.path.getmtime(self.src.path)
+        out_file = os.path.join(self.cur_out_dir, f"{self.src.name}.cur")
         # Only generate the file if the SVG is newer than the CUR
         if os.path.exists(out_file) and os.path.getmtime(out_file) > src_file_mtime:
             print(gray(f"Skipped {out_file}"))
@@ -238,20 +427,20 @@ class CursorGenerator:
         icondir = bytearray()
         icondir.extend((0).to_bytes(2, "little"))
         icondir.extend((2).to_bytes(2, "little"))
-        icondir.extend(len(self.res).to_bytes(2, "little"))
+        icondir.extend(len(self.resolutions).to_bytes(2, "little"))
 
         images = []
-        for res in self.res:
+        for res in self.resolutions:
             png = os.path.join(self.png_out_dir, f"{res}.png")
             images.append(self.__gen_bitmap(png, res))
 
         entries = []
         ENTRY_SIZE = 16
-        min_offset = len(icondir) + ENTRY_SIZE*len(self.res)
-        x, y = self.__get_hotspot()
-        for i, res in enumerate(self.res):
-            hotspotX = int(res * x)
-            hotspotY = int(res * y)
+        min_offset = len(icondir) + ENTRY_SIZE*len(self.resolutions)
+
+        for i, res in enumerate(self.resolutions):
+            hotspotX = int(res * self.src.hotspot.x)
+            hotspotY = int(res * self.src.hotspot.y)
             image_offset = min_offset + sum(map(len, images[:i]))
             entry = bytearray()
             entry.extend(u8(res))              # bWidth
@@ -330,44 +519,9 @@ class CursorGenerator:
 
         return image_data
 
-    def __get_hotspot(self) -> tuple[float, float]:
-        """Get the hotspot where (0, 0) is top left and (1, 1) is bottom right"""
-        svg_tree = xml.parse(self.src_svg).getroot()
-        width = int(svg_tree.attrib["width"])
-        height = int(svg_tree.attrib["height"])
-
-        x_attrib = None
-        y_attrib = None
-
-        for element in svg_tree:
-            if element.get('id') == 'hotspot' and element.tag.endswith('rect'):
-                x_attrib = element.get('x')
-                y_attrib = element.get('y')
-
-        if x_attrib is None:
-            x = 0
-        else:
-            try:
-                x = int(x_attrib)
-            except (ValueError, TypeError):
-                x = 0
-
-        if y_attrib is None:
-            y = 0
-        else:
-            try:
-                y = int(y_attrib)
-            except (ValueError, TypeError):
-                y = 0
-
-        x /= width
-        y /= height
-
-        return min(max(x, 0), 1), min(max(y, 0), 1)
-
     async def generate(self):
-        await self.__gen_pngs()
-        self.__gen_cur()
+        if (await self.__gen_pngs()):
+            self.__gen_cur()
 
 
 async def main():
@@ -375,16 +529,23 @@ async def main():
         print(red("Inkscape is required to use 'generate.py'"))
         exit(1)
 
-    tasks = []
+    coroutines = []
     for file in os.listdir("svgs"):
+        try:
+            cursor = Cursor(os.path.join("svgs", file))
+        except CursorError:
+            continue
         generator = CursorGenerator(
-            os.path.abspath(os.path.join("svgs", file)),
-            "pngs",
-            "cursors",
-            res=(32, 48, 64)
+            cursor,
+            png_out_dir="pngs",
+            cur_out_dir="cursors",
+            resolutions=(32, 48, 64)
         )
-        tasks.append(generator.generate())
-    await asyncio.gather(*tasks)
+        coroutines.append(generator.generate())
+
+    MAX_CONCURRENT_TASKS = 8
+    for i in range(0, len(coroutines), MAX_CONCURRENT_TASKS):
+        await asyncio.gather(*coroutines[i:i + MAX_CONCURRENT_TASKS])
 
 
 if __name__ == "__main__":
